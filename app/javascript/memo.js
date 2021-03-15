d3ce3e7232a3640b7d16aa51e305d3a979808d60
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-03-15 19:19:43 +0900

    修正

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,14 +1,14 @@
 const buildHTML = (XHR) => {
   const item = XHR.response.post;
-      const html = `
-        <div class="post">
-          <div class="post-date">
-            投稿日時：${item.created_at}
-          </div>
-          <div class="post-content">
-            ${item.content}
-          </div>
-        </div>`;
+  const html = `
+    <div class="post">
+    <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
   return html
 }
 
